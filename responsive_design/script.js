d14375a3b1bdc30b6922b3366db762188abb0d6d
Ariina-07 <redkina.y021@gmail.com>
2025-10-10 16:19:10 +0500

    Добавлена ПР6

--- responsive_design/script.js
+++ responsive_design/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const mobileMenu = document.querySelector('.nav-mobile');
     
-    menuToggle.addEventListener('click', function() {
+    menuToggle.addEventListener('click', function() { 
         mobileMenu.classList.toggle('active');
     });
 });
